53bb4f2ca57eaf9fae735d7b44d47fe7427da219
esther <esther@yahoo.com>
2022-07-25 08:26:11 +0100

    test

--- modules/Compute/main.tf
+++ modules/Compute/main.tf
@@ -16,38 +16,3 @@ resource "aws_instance" "Jenkins" {
 }
 
 
-#create instance for sonbarqube
-resource "aws_instance" "sonbarqube" {
-  ami                         = var.ami-sonar
-  instance_type               = var.instance-type-artifact-sonar
-  subnet_id                   = var.subnets-compute
-  vpc_security_group_ids      = var.sg-compute
-  associate_public_ip_address = true
-  key_name                    = var.keypair
-
-
-   tags = merge(
-    var.tags,
-    {
-      Name = "ACS-SONARQUBE"
-    },
-  )
-}
-
-# create instance for artifactory
-resource "aws_instance" "artifactory" {
-  ami                         = var.ami-jfrog
-  instance_type               = var.instance-type-artifact-sonar
-  subnet_id                   = var.subnets-compute
-  vpc_security_group_ids      = var.sg-compute
-  associate_public_ip_address = true
-  key_name                    = var.keypair
-
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "ACS-ARTIFACTORY"
-    },
-  )
-}
